4259ec5507015edcfe1d18e93c9dc4c19b7058fe
jahid-wm4d <88807546+jahid-wm4d@users.noreply.github.com>
2022-10-13 12:49:21 +0600

    fghjkl

--- src/QuizDetails.js
+++ src/QuizDetails.js
@@ -49,30 +49,44 @@ let ShowQuiz = ({quiz}) => {
             <h2 className='text-white text-xl mt-4'>{quiz.question}</h2>
  
             {
-                quiz.options.map(option => <QuizOption option= {option}></QuizOption>)
+                quiz.options.map(option => <QuizOption option= {option} quiz = {quiz}></QuizOption>)
             }
 
             {
                 showCorrectAns && <h2 className='text-cyan-500 text-xl font-extrabold'>Correct Answer : {quiz.correctAnswer}</h2>
             }
             
-            <button className="btn btn-primary mt-5" onClick={() => setAnswer(true)}>Button</button>
+            <button className="btn btn-primary mt-5" onClick={() => setAnswer(true)}>See Correct Answer</button>
 
         </div>
             
     )
 }
-let QuizOption = ({option}) => {
+let QuizOption = ({option, quiz}) => {
+    // let [seeCorrectAnswer, setCorrectAnswer] = useState(false);
+    let showCorrectAns = () => {
+        // console.log(quiz.correctAnswer)
+        if(option === quiz.correctAnswer){
+            let callCorrectAns = document.getElementById('correctAns');
+            callCorrectAns.innerText = 'Correct';
+            console.log('correct')
+        }
+        else{
+            let callCorrectAns = document.getElementById('correctAns');
+            callCorrectAns.innerText = 'Incorrect';
+        }
+    }
     return(
         <div>
-
-        <div className='bg-slate-100 mt-5 '>
+        <button className='bg-slate-100 mt-5 w-full' onClick={showCorrectAns}>
             <div className='flex justify-center items-center'>
-            <input type="radio" name="radio-1" className="radio mr-3 radio-accent" />
-            <button className='text-red-700 mt-3 mb-3'>{option}</button>
-
+            {/* <input type="radio" name="radio-1" className="radio mr-3 radio-accent" /> */}
+            <p className='text-red-700 mt-3 mb-3'>{option}</p>
+            
             </div>
-        </div>
+            <h2 className='text-2xl text-emerald-700 font-mono'  id='correctAns'></h2>
+            
+        </button>
         
         </div>
     )
